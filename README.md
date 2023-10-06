d28bf0ea66e910a42f50715c624fe44e2c4d4adb
cieloenfer <105323721+cieloenfer@users.noreply.github.com>
2023-10-05 23:49:26 -0300

    subida

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# EntregaFinalReactFlores
-E-Commerce legal que ofrece ayudas extrajudiciales 
+# Iuris Help 
+*"E-comerce dedicada a ofrecer servicios legales para una sana convivencia, ya sea desde el ámbito
+Servicios Jurídicos 
+o en la gestión de conflictos"* 
+
+## Servicios Ofrecidos: 
+1. Mediación Familiar 
+2. Mediación Laboral 
+3. Mediación Escolar 
+4. Asesoría Legal a Condominios
+5. Redacción de Escritos
+
+## Uso:
+Para usar este proyecto debe tener instalado NodeJS y npm (Node Package Manager)
+
+El usuario debe descargar la aplicación comercial y podrá comenzar a contratar los servicios. 
+
+## Lenguajes Ocupados: 
+HTML - CSS - REACT JS 
---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import * as React from 'react'
+import  Navbar from  "./components/Nabvar/Navbar";
+import Title from './components/Title';
+import itemListContainer from "./components/itemListContainer";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import ItemDetailContainrer from "./components/ItemDetailContainer";
+import { ChakraProvider } from '@chakra-ui/react';
+import './App.css';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRooter>
+      <Navbar/>
+   <ChakraProvider/>
+   <h1>Home</h1>
+    <route path="/" element={<HOME/>}/>
+      <Routes>
+        <Route exact path = "/" element = {<ItemListContainer />} />
+        <Route exact path = '/category/:categoryName' element = 
+        {<ItemListContainer />} />
+      </Routes>
+   <ItemDetailContainrer/>
+      </BrowserRooter>
+  
+     </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+  
+ 
+     
